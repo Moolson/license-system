db6b429d7ceb57ce9f2f6629a83b9bc66f7da452
Moolson <94926450+Moolson@users.noreply.github.com>
2023-01-31 17:55:07 +0100

    Update licensecheck.js

--- license-system/functions/licensecheck.js
+++ license-system/functions/licensecheck.js
@@ -14,13 +14,14 @@ function checkifexpires(result, check){
     return { works, expires }
 }
 function checkifexists(req, res) {
-    connection.query(`SELECT * FROM licenses WHERE license = '${req.params.check}'`, function( err, result ) {
+    const license = req.params.check
+    connection.query(`SELECT * FROM licenses WHERE license = ?`, [license], function( err, result ) {
         if(err) throw err;
         if(result && result.length){
             const check = true;
             const  { works, expires } = checkifexpires(result, check)
                 res.json({
-                    "id": result[0].id,
+                    id: result[0].id,
                     "license":result[0].license,
                     "type":result[0].type,
                     "owner": result[0].owner,
@@ -29,7 +30,7 @@ function checkifexists(req, res) {
                 }) 
             } else {
             res.json({
-                "id": "none",
+                id: "none",
                 "license":"none",
                 "type":"none",
                 "owner": "none",
